3d1ac38e898244342c861074c25974e753209168
tj-liebsch-admin <tjliebsch@outlook.com>
2023-10-25 14:57:17 -0500

    I added a functional drag and drop system

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,34 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour
+public class DragAndDrop : MonoBehaviour, IDragHandler
 {
-    private RectTransform draggingObject;
+    //DampingSpeed conntrols how fast the object can move (It allows the object to drag behind the mouse)
+    [SerializeField]
+    private float dampingSpeed = 0.05f;
+
+    //This is based off the object that holds the C# file
+    private RectTransform draggingObjectRectTransform;
+    private Vector3 velocity = Vector3.zero;
     
     private void Awake()
     {
         //changes the type of transform into RectTransform
-        draggingObject = transform as RectTransform; 
+        //allows the draggingObject to store the transform of the object that holds the C# file
+        draggingObjectRectTransform = transform as RectTransform; 
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (RectTransformUtility.ScreenPointToWorldPointInRectangle())
+        //If the mouse clicks the object then move it
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingObjectRectTransform, eventData.position, eventData.pressEventCamera, out var globalMousePosition))
         {
-
+            draggingObjectRectTransform.position = Vector3.SmoothDamp(draggingObjectRectTransform.position, globalMousePosition, ref velocity, dampingSpeed);
         }
     }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Are you working?
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
